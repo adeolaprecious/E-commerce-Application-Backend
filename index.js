c91e32750f28973c7ad749954f1ccfe73e2a0997
Adebowale Adeola Precious <adeolaprecious006@gmail.com>
2025-09-30 09:56:54 +0100

    commit two

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express()
 const port = 4950;
 const ejs = require("ejs");
 app.set('view engine', 'ejs');
-    
-app.use(express.urlencoded({extended: true}))
-// app.set('views', __dirname + '/models/views');
 const mongoose = require("mongoose");
+
+const customerRouter = require ('./routes/user.route')
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const URI = process.env.URI
 
 
 let URI = "mongodb+srv://adeolaprecious:pearl10@cluster0.abcw6bw.mongodb.net/project?retryWrites=true&w=majority&appName=Cluster0"
@@ -23,7 +27,17 @@ app.get("/signup", (req,res)=>{
     res.render("signup")
 })
 
+
+
+let allCustomers = []
+
+app.use("/user", customerRouter);
+
+const port = process.env.PORT || 4950 ;
+
 app.listen(port, ()=>{
     console.log(`Server has started on port ${port}`);
     
 })
+
+
