f643ec809404ad78e79df9a04b6af6ec6a996159
Adebowale Adeola Precious <adeolaprecious006@gmail.com>
2025-10-02 11:02:13 +0100

    feat: implement user authentication routes and views
    - Added user registration and login routes in user.route.js
    - Created signup and signin views using EJS templates
    - Implemented LRUCache, parseOptions, and regex patterns for semver
    - Updated package.json for semver with necessary scripts and dependencies
    - Added range parsing and validation functionalities in semver

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let customerSchema = mongoose.Schema({
     firstName: {type: String, required: true},
-    secondName: {type: String, required: true},
+    lastName: {type: String, required: true},
     email: {type: String, required: true , unique: [true, "Email has been taken, please choose another one"]},
     password: {type: String, required: true}
 });
---
+++ routes/user.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getSignup,
+  postRegister,
+  getSignin,
+  postLogin,
+} = require("../controllers/user.controller");
+
+// signup
+router.get("/signup", getSignup);
+router.post("/register", postRegister);
+
+// signin
+router.get("/signin", getSignin);
+router.post("/login", postLogin);
+
+module.exports = router;
